80bd0281348f39f9640563f4bfea58ee80ebdd07
rtxyd <65117312+rtxyd@users.noreply.github.com>
2024-10-03 18:17:41 +0800

    添加项目文件。

---
+++ 1.5/Source/MinchoWitchPatch/CheckHediffSeverity.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+
+namespace MinchoWitchPatch
+{
+    public class CompCheckHediffSeverity : ThingComp
+    {
+        public CompProperties_CheckHediffSeverity Props => (CompProperties_CheckHediffSeverity)this.props;
+        private int tickCounter = 0;
+
+        public override void CompTick()
+        {
+            tickCounter++;
+            if (tickCounter >= Props.tickInterval)
+            {
+                tickCounter = 0;
+                Pawn pawn = this.parent as Pawn;
+                if (pawn != null)
+                {
+                    Hediff targetHediff = pawn.health.hediffSet.GetFirstHediffOfDef(Props.targetHediff);
+                    if (targetHediff != null)
+                    {
+                        float newSeverity = targetHediff.Severity;
+                        Hediff ownHediff = pawn.health.hediffSet.GetFirstHediffOfDef(Props.ownHediff);
+                        if (ownHediff != null)
+                        {
+                            ownHediff.Severity = newSeverity;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 1.5/Source/MinchoWitchPatch/CompProperties_CheckHediffSeverity.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace MinchoWitchPatch
+{
+    public class CompProperties_CheckHediffSeverity : CompProperties
+    {
+        public HediffDef targetHediff;
+        public HediffDef ownHediff;
+        public int tickInterval = 250;
+
+        public CompProperties_CheckHediffSeverity()
+        {
+            this.compClass = typeof(CompCheckHediffSeverity);
+        }
+    }
+}
